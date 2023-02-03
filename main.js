35db2f70e4e579985001eb0b96777107cf9f196d
Drew Coker <drewrosecoker@gmail.com>
2023-02-03 14:55:52 -0500

    update to everything

--- main.js
+++ main.js
@@ -1,102 +1,108 @@
-import API_KEY from "./api.js"; 
+import API_KEY from "./api.js";
 
 const BASE_URL = "https://api.github.com/users/drewrc";
+const ORG_URL = "https://api.github.com/users/drewrc/orgs"
 
 //const avatar
 //const followers =
 //const repos = "repos_url"
 
 function generateHTML(data) {
-    console.log(data);
+  console.log(data.created_at);
 
-    const source = document.querySelector("#profile-card").innerHTML;
-    const template = Handlebars.compile(source);
+  const source = document.querySelector("#profile-card").innerHTML;
+  const template = Handlebars.compile(source);
 
-    const html = template(data);
-    document
+  const html = template(data);
+  document
     .querySelector(".side-profile")
     .insertAdjacentHTML("afterbegin", html);
+
 }
+
 
 
 //function reposList(list) {
- //   console.log(list);
+//   console.log(list);
 
- //   const source = document.querySelector("#repos-list").innerHTML;
- //   const template = Handlebars.compile(source);
+//   const source = document.querySelector("#repos-list").innerHTML;
+//   const template = Handlebars.compile(source);
 
- //   const html = template(list);
- //   document
- //   .querySelector(".side-profile")
-  //  .insertAdjacentHTML("afterbegin", html);
+//   const html = template(list);
+//   document
+//   .querySelector(".side-profile")
+//  .insertAdjacentHTML("afterbegin", html);
 //}
 
-
-
-
-
-fetch(`${BASE_URL}${"/repos"}`)
-.then(function (response) {
+fetch(`${BASE_URL}/repos`)
+  .then(function (response) {
     return response.json();
-})
-//.then((response) => response.json())
-//json -> javascript object notation
-.then(function (list) {
-  console.log(list)
-  console.log(list[.0].name)
-  console.log(list[1].name)
-//    reposList(list);
+  })
+  .then(function (list) {
+    reposList(list);
+    console.log(list);
+  });
 
-});
-
-//repo info and organization 
+//repo info and organization
 
 function reposList(list) {
-   // console.log(list);
+  // console.log(list);
+
+  const source = document.querySelector("#repos-list").innerHTML;
+  const template = Handlebars.compile(source);
+
+  const html = template({ repos: list });
+  document.querySelector(".repo-container").insertAdjacentHTML("afterbegin", html);
+
   
-    const source = document.querySelector("#repos-list").innerHTML;
-    const template = Handlebars.compile(source);
-
-    const html = template(list);
-    document
-    .querySelector(".repos-list")
-    .insertAdjacentHTML("afterbegin", html);
-
 }
 
+fetch(`${ORG_URL}`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (orgs) {
+    organization(orgs);
+    console.log(orgs)
+  });
 
-//fetch(`${BASE_URL}${"/followers"}`)
-//.then(function (response) {
- //   return response.json();
-//})
+//repo info and organization
 
-//.then(function (followers) {
- // console.log(followers)
-//});
+function organization(orgs) {
+ console.log(orgs);
+
+  const source = document.querySelector("#orgs-list").innerHTML;
+  const template = Handlebars.compile(source);
+
+  const html = template({ organizations: orgs });
+  document.querySelector(".org-icons").insertAdjacentHTML("afterbegin", html);
+}
+
 
 
 fetch(`${BASE_URL}`)
-.then(function (response) {
+  .then(function (response) {
     return response.json();
-})
-//.then((response) => response.json())
-//json -> javascript object notation
-.then(function (user) {
-  console.log(user)
-    userInfo(user);
-});
-
-
-function userInfo(user) {
+  })
+  //.then((response) => response.json())
+  //json -> javascript object notation
+  .then(function (user) {
     console.log(user);
+    userInfo(user);
+  });
 
-    const source = document.querySelector("#profile-card").innerHTML;
-    const template = Handlebars.compile(source);
+ // const created = user.created_at
+ // console.log(created)
 
-    const html = template(user);
-    document
+
+ function userInfo(user) {
+  console.log(user);
+
+  const source = document.querySelector("#profile-card").innerHTML;
+  const template = Handlebars.compile(source);
+
+  const html = template(user);
+  document
     .querySelector(".side-profile")
     .insertAdjacentHTML("afterbegin", html);
-
 }
-
